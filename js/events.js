095b3ec28adb7584538e02602cf95a521c26d5a6
Lautaro <lautygz@outlook.com>
2022-04-26 18:11:22 -0300

    JQuery added

--- js/events.js
+++ js/events.js
@@ -1,18 +1,37 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     /* This code it's used to animate scroll button up */
-
+    /*
     const buttonUp = document.getElementById('button-up')
 
     window.onscroll = function(){scrollFunction()};
 
     function scrollFunction(){
-        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
             buttonUp.style.display = "block";
         }else{
             buttonUp.style.display = "none";
         }
     }
+    */
+
+    $(document).ready(function(){
+
+        $('.button-up').click(function(){
+            $('body, html').animate({
+                scrollTop: '0px'
+            }, 300);
+        });
+    
+        $(window).scroll(function(){
+            if( $(this).scrollTop() > 0 ){
+                $('.button-up').slideDown(300);
+            } else {
+                $('.button-up').slideUp(300);
+            }
+        });
+    
+    });
 
     /* This code it's used for animate the items of nav menu */
 
