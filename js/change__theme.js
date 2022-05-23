11b1477cd8c3f9b67f64f3290d4e397e0f6bff8e
Lautaro <lautygz@outlook.com>
2022-05-23 14:51:19 -0300

    Update change__theme.js

--- js/change__theme.js
+++ js/change__theme.js
@@ -3,45 +3,17 @@ document.addEventListener('DOMContentLoaded', function(){
     /* This code change the theme color */
 
     const changeDM = document.getElementById('change-dk-mode'),
-          changeLM = document.getElementById('change-lg-mode'),
+          changeLM = document.getElementById('change-lg-mode')
 
-    const body = document.querySelector('body'),
-          initialTheme = 'light'
-
-    const setTheme = (theme) => {
-        localStorage.setItem('theme', theme)
-        body.setAttribute('data-theme', theme)
-    }
-
-    const toggleTheme = () => {
-        const activeTheme = localStorage.getItem('theme');
-
-        if (activeTheme === 'light'){
+    if (changeDM){
             setTheme('dark')
             changeDM.classList.add('remove-theme-toggle')
             changeLM.classList.remove('show-theme-toggle')
-        }
-        else{
+    }
+    else{
             setTheme('light');
             changeLM.classList.add('remove-theme-toggle')
             changeDM.classList.remove('show-theme-toggle')
-        }
     }
-
-    const setThemeOnInit = () => {
-        const savedTheme = localStorage.getItem('theme');
-        savedTheme
-            ? body.setAttribute('data-theme', savedTheme)
-            : setTheme(initialTheme);
-    }
-
-    setThemeOnInit()
-
-    if(changeDM || changeLM){
-        changeDM.addEventListener('click', () => toggleTheme())
-        changeLM.addEventListener('click', () => toggleTheme())
-    }
-
-
 
 })
