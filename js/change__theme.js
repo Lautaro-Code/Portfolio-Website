21e79fe6862593f9dff7837a67e6abe0c76d6c35
Lautaro <lautygz@outlook.com>
2022-05-23 18:43:15 -0300

    change theme

--- js/change__theme.js
+++ js/change__theme.js
@@ -2,28 +2,14 @@ document.addEventListener('DOMContentLoaded', function(){
 
     /* This code change the theme color */
 
-    const changeDM = document.getElementById('change-dk-mode'),
-          changeLM = document.getElementById('change-lg-mode')
-
-    if (changeDM){
-            setTheme('dark')
-            changeDM.classList.add('remove-theme-toggle')
-            changeLM.classList.remove('show-theme-toggle')
-    }
-    else{
-            setTheme('light');
-            changeLM.classList.add('remove-theme-toggle')
-            changeDM.classList.remove('show-theme-toggle')
-    }
-
-    setTheme(theme) = () => {
-        if(theme === 'dark'){
-            document.documentElement.style.setproperty('--main-bg', 'rgb(45, 37, 50)');
-            document.documentElement.style.setproperty('--main-text', '#f5f5f5');
-        }else{
-            document.documentElement.style.setproperty('--main-text', 'rgb(45, 37, 50)');
-            document.documentElement.style.setproperty('--main-bg', '#f5f5f5');
-        }
-    }
+    const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
+            function cambiaTema(ev){
+                if(ev.target.checked){
+                    document.documentElement.setAttribute('theme', 'light');
+                } else {
+                    document.documentElement.setAttribute('theme', 'dark');
+                }
+            }
+            colorSwitch.addEventListener('change', cambiaTema);
 
 })
