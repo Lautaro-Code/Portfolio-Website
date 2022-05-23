5a645c88d4400bfe31c5bf4c5457c12fe50a26b4
Lautaro <lautygz@outlook.com>
2022-05-23 14:59:17 -0300

    Change theme

--- js/change__theme.js
+++ js/change__theme.js
@@ -16,4 +16,14 @@ document.addEventListener('DOMContentLoaded', function(){
             changeDM.classList.remove('show-theme-toggle')
     }
 
+    setTheme(theme) = () => {
+        if(theme === 'dark'){
+            document.documentElement.style.setproperty('--main-bg', 'rgb(45, 37, 50)');
+            document.documentElement.style.setproperty('--main-text', '#f5f5f5');
+        }else{
+            document.documentElement.style.setproperty('--main-text', 'rgb(45, 37, 50)');
+            document.documentElement.style.setproperty('--main-bg', '#f5f5f5');
+        }
+    }
+
 })
